853fcde0a498bbe93ae5875d31b5e2a194696718
Chris Forrest <redrider_cr@yahoo.com>
2020-08-28 19:47:35 -0400

    projects table created

--- data/migrations/20200828192033_sprint-tables.js
+++ data/migrations/20200828192033_sprint-tables.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("projects", (tbl) => {
+      tbl.increments() //this makes the id unique not null and required
+      tbl.text("name",220).notNullable()
+      tbl.text("description")
+      tbl.boolean("completed").defaultTo(false)
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("projects")
 };

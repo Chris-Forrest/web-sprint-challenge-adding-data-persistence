b44935ad3b9d0d1b81f1f3c7f47ccb14f60f25d6
Chris Forrest <redrider_cr@yahoo.com>
2020-08-28 20:02:14 -0400

    completed tasks table

--- data/migrations/20200828192033_sprint-tables.js
+++ data/migrations/20200828192033_sprint-tables.js
@@ -13,10 +13,27 @@ exports.up = function(knex) {
     tbl.text("description")
   })
 
+  .createTable("tasks", (tbl) => {
+    tbl.increments()
+    tbl.text("description").notNullable()
+    tbl.text("notes")
+    tbl.boolean("completed").defaultTo(false)
+    tbl.integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE")
+  })
+
+
+
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists("tasks")
     .dropTableIfExists("resources")
     .dropTableIfExists("projects")
 };
